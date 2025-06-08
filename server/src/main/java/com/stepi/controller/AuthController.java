35836437392a9faf81fa218251a40c590e876e9b
etd93 <etd93@naver.com>
2025-06-08 23:59:51 +0900

    [feat] : Add logging to authentication controller

--- server/src/main/java/com/stepi/controller/AuthController.java
+++ server/src/main/java/com/stepi/controller/AuthController.java
@@ -1,32 +1,31 @@
 package com.stepi.controller;
 
-import com.stepi.model.User;
-import com.stepi.service.UserService;
+import com.stepi.dto.RegisterRequest;
+import com.stepi.dto.LoginRequest;
+import com.stepi.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@RequiredArgsConstructor
 public class AuthController {
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
-    @Autowired
-    private UserService userService;
+    private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody User user) {
-        try {
-            logger.debug("Received registration request for user: {}", user.getEmail());
-            User registeredUser = userService.registerUser(user);
-            logger.debug("User registered successfully: {}", registeredUser.getEmail());
-            return ResponseEntity.status(201).body(registeredUser);
-        } catch (Exception e) {
-            logger.error("Registration failed for user: " + user.getEmail(), e);
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        logger.info("Received registration request: {}", request);
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        logger.info("Received login request: {}", request);
+        return authService.login(request);
     }
 }
