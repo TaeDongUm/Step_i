4f0805d02e6ccb0117dc6fb423e5597c8959be0f
etd93 <etd93@naver.com>
2025-06-08 23:57:38 +0900

    [fix] : delete cors settings

--- server/src/main/java/com/stepi/config/WebConfig.java
+++ server/src/main/java/com/stepi/config/WebConfig.java
@@ -1,21 +1,9 @@
 package com.stepi.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowedHeaders("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin",
-                        "Access-Control-Request-Method", "Access-Control-Request-Headers")
-                .exposedHeaders("Authorization")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+    // CORS 설정 제거
 }
